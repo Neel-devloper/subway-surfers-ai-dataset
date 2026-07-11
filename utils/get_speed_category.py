cabee24adc74081122aa7d7b57100c8d1907ed96
Neel-devloper <neel.vorani@gmail.com>
2026-07-11 18:47:33 -0400

    Strip repo down to dataset only, remove bot code/docs/config

--- utils/get_speed_category.py
+++
@@ -1,9 +0,0 @@
-from config import *
-
-def get_speed_category(elapsed_seconds):
-    if elapsed_seconds < SLOW_THRESHOLD:
-        return SLOW_FOLDER
-    elif elapsed_seconds < MEDIUM_THRESHOLD:
-        return MEDIUM_FOLDER
-    else:
-        return FAST_FOLDER
